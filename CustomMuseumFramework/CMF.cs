8d4a85523dc5547781fe13b56425731ea48d7dc6
--unset <azurylldmm@gmail.com>
2024-10-11 18:02:47 -0400

    Rename entry .cs to CMF.cs. Added SpaceCore API.

--- CustomMuseumFramework/CMF.cs
+++ CustomMuseumFramework/CMF.cs
@@ -6,14 +6,16 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 using CustomMuseumFramework.Helpers;
+using SpaceShared.APIs;
 
 namespace CustomMuseumFramework
 {
-    internal sealed class CustomMuseums : Mod
+    internal sealed class CMF : Mod
     {
         internal static IModHelper ModHelper { get; set; } = null!;
         internal static IMonitor ModMonitor { get; set; } = null!;
         internal static Harmony Harmony { get; set; } = null!;
+        internal static ISpaceCoreApi? SpaceCoreAPI { get; set; } = null!;
 
         public override void Entry(IModHelper helper)
         {
@@ -23,7 +25,19 @@ namespace CustomMuseumFramework
 
             Harmony.PatchAll();
 
+            Helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
             Helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            SpaceCoreAPI = ModHelper.ModRegistry.GetApi<ISpaceCoreApi>("spacechase0.SpaceCore");
+            if (SpaceCoreAPI == null)
+            {
+                Log.Error("SpaceCore not found. This mod requires it to run.");
+                return;
+            }
+            Log.Alert("Found SpaceCore API!");
         }
 
         private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
--- CustomMuseumFramework/Helpers/Log.cs
+++ CustomMuseumFramework/Helpers/Log.cs
@@ -7,27 +7,27 @@ namespace CustomMuseumFramework.Helpers;
 
 public static class Log
 {
-    public static void Debug<T>(T message) => CustomMuseums.ModMonitor.Log(
+    public static void Debug<T>(T message) => CMF.ModMonitor.Log(
         $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
         LogLevel.Debug);
 
-    public static void Error<T>(T message) => CustomMuseums.ModMonitor.Log(
+    public static void Error<T>(T message) => CMF.ModMonitor.Log(
         $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
         LogLevel.Error);
 
-    public static void Warn<T>(T message) => CustomMuseums.ModMonitor.Log(
+    public static void Warn<T>(T message) => CMF.ModMonitor.Log(
         $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
         LogLevel.Warn);
 
-    public static void Info<T>(T message) => CustomMuseums.ModMonitor.Log(
+    public static void Info<T>(T message) => CMF.ModMonitor.Log(
         $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
         LogLevel.Info);
 
-    public static void Trace<T>(T message) => CustomMuseums.ModMonitor.Log(
+    public static void Trace<T>(T message) => CMF.ModMonitor.Log(
         $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
         LogLevel.Trace);
 
-    public static void Alert<T>(T message) => CustomMuseums.ModMonitor.Log(
+    public static void Alert<T>(T message) => CMF.ModMonitor.Log(
         $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
         LogLevel.Alert);
 
