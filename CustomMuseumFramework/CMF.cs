b0b5a2d760b2f456787ab44f8804d4efd1a9a4d0
Spiderbuttons <azurylldmm@gmail.com>
2025-06-06 23:26:06 -0400

    Adjusted amount of comment slashes for very important reasons.

--- CustomMuseumFramework/CMF.cs
+++ CustomMuseumFramework/CMF.cs
@@ -220,7 +220,7 @@ namespace CustomMuseumFramework
         {
             if (e.Button is SButton.F2)
             {
-                //
+                //////////
             }
         }
     }
