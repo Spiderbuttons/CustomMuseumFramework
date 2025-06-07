d6dafaeb33f5763f28bb56064356c62a00fbecd8
Spiderbuttons <azurylldmm@gmail.com>
2025-06-06 22:11:37 -0400

    Prefix skip Item.canStackWith to prevent items with CMF_Position data from stacking with similar items.

---
+++ CustomMuseumFramework/Patches/ItemPatches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using StardewValley;
+
+namespace CustomMuseumFramework.Patches;
+
+[HarmonyPatch(typeof(Item))]
+public static class ItemPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(Item.canStackWith))]
+    public static bool canStackWith_Prefix(Item __instance, ref bool __result, ISalable other)
+    {
+        if (__instance.modData.ContainsKey("CMF_Position") || (other is Item item && item.modData.ContainsKey("CMF_Position")))
+        {
+            __result = false;
+            return false;
+        }
+
+        return true;
+    }
+}
