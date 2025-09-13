b464f401610de54b6bfc912601c2a6eed8ec2a74
Spiderbuttons <azurylldmm@gmail.com>
2025-09-13 18:29:17 -0400

    Custom JsonConverter for Newtonsoft to accept single items for fields that should be lists of items.

---
+++ CustomMuseumFramework/Helpers/SingleOrListConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CustomMuseumFramework.Helpers;
+
+public class SingleOrListConverter<T> : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(List<T>);
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        JToken token = JToken.Load(reader);
+        return token.Type switch
+        {
+            JTokenType.Null => null,
+            JTokenType.Array => token.ToObject<List<T>>(),
+            _ => new List<T> { token.ToObject<T>()! }
+        };
+    }
+
+    public override bool CanWrite => false;
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
